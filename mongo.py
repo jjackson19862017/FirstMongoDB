924c60bf9d34ced0437222251d06d9812f08488d
Stephen Jackson <jjackson19862017@gmail.com>
2020-01-28 00:25:34 +0000

    Help
    Signed-off-by: Stephen Jackson <jjackson19862017@gmail.com>

---
+++ mongo.py
@@ -0,0 +1,27 @@
+import pymongo
+import os
+
+MONGODB_URI = os.getenv("OLDERMONGO_URI")
+DBS_NAME = "MyTestDB"
+COLLECTION_NAME = "myFirstMDB"
+
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MondoDB: %s") % e
+
+
+conn = mongo_connect(MONGODB_URI)
+
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+documents = coll.find()
+
+
+for doc in documents:
+    print(doc)
