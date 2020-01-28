e57ffca8e9f19c48b582849dc86b2d53d570f63f
Stephen Jackson <jjackson19862017@gmail.com>
2020-01-28 00:45:45 +0000

    Working
    Signed-off-by: Stephen Jackson <jjackson19862017@gmail.com>

--- mongo.py
+++ mongo.py
@@ -1,8 +1,12 @@
 import pymongo
 import os
 
-MONGODB_URI = os.getenv("OLDERMONGO_URI")
-DBS_NAME = "MyTestDB"
+from os import path
+if path.exists("env.py"):
+    import env
+
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "myTestDB"
 COLLECTION_NAME = "myFirstMDB"
 
 
